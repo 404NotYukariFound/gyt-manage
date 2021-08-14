b4ee409432fd42eb8156a612373e1a84db2857a8
Kele_Bingtang <2456019588@qq.com>
2021-08-14 17:46:03 +0800

    1.添加首页

--- src/main/resources/static/project/js/index.js
+++ src/main/resources/static/project/js/index.js
@@ -5,8 +5,28 @@
         })
         
     })*/
-    $("#resource-manage").on("click",function (){
-        $("iframe").attr("src","/eight/sourceManage")
-    })
-
+    //选中后的样式
+    function menuSelected() {
+        let item = $(".menu").find("dd")
+        for (let i = 0; i < item.length; i++) {
+            $(item[i]).on("click",function (){
+                $(this).addClass("menu-selected");
+                item.not(item[i]).removeClass("menu-selected");
+            })
+        }
+    }
+    menuSelected();
+    /*
+    参数1：菜单栏的id值
+    参数2：跳转的路径
+    */
+    function toCustomPage(ele,src){
+        let target = '#' + ele;
+        $(target).on("click",function (){
+            $("iframe").attr("src",src)
+        })
+    }
+    
+    toCustomPage("resource-manage","/eight/sourceManage")
+    
 })()
