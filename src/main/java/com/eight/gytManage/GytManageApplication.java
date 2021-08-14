ffafc1c480bf2b3f51a62742e8466a711e428336
Kele_Bingtang <2456019588@qq.com>
2021-08-14 12:06:38 +0800

    1.加入插件

--- src/main/java/com/eight/gytManage/GytManageApplication.java
+++ src/main/java/com/eight/gytManage/GytManageApplication.java
@@ -1,9 +1,12 @@
 package com.eight.gytManage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@MapperScan("com.eight.gytManage.mapper")
 public class GytManageApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/eight/gytManage/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.eight.gytManage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author Kele-Bing
+ * @Create 2021/8/14 11:51
+ * @Version 1.0
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String toLoginPage(){
+        return "index";
+    }
+
+    @RequestMapping("/index")
+    public String toIndexPage(){
+        return "index";
+    }
+}
