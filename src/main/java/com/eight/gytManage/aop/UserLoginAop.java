aba7d90ffca0641068c2b0a68fb55dede8316fa6
Kele_Bingtang <2456019588@qq.com>
2021-08-14 11:37:47 +0800

    1.加入插件

---
+++ src/main/java/com/eight/gytManage/aop/UserLoginAop.java
@@ -0,0 +1,9 @@
+package com.eight.gytManage.aop;
+
+/**
+ * @Author Kele-Bing
+ * @Create 2021/8/14 11:37
+ * @Version 1.0
+ */
+public class UserLoginAop {
+}
---
+++ src/main/java/com/eight/gytManage/mapper/a.java
@@ -0,0 +1,9 @@
+package com.eight.gytManage.mapper;
+
+/**
+ * @Author Kele-Bing
+ * @Create 2021/8/14 11:36
+ * @Version 1.0
+ */
+public class a {
+}
